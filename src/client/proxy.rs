8fe07710dd68dc735b7112ff8d7932c59747ee7a
pboymt <10893582+pboymt@users.noreply.github.com>
2024-02-15 16:41:50 +0800

    feat: 调整代理设置结构便于读取

--- src/client/proxy.rs
+++ src/client/proxy.rs
@@ -1,17 +1,27 @@
+use serde::{Deserialize, Serialize};
+
 /// EhClient 代理
-#[derive(Debug, Clone)]
-pub struct EhClientProxy(String, String, i32);
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct EhClientProxy {
+    pub protocol: String,
+    pub host: String,
+    pub port: i32,
+}
 
 impl EhClientProxy {
     /// 创建一个新的 EhClientProxy 实例
-    pub fn new(schema: &str, host: &str, port: i32) -> Self {
-        EhClientProxy(schema.to_string(), host.to_string(), port)
+    pub fn new(protocol: &str, host: &str, port: i32) -> Self {
+        EhClientProxy {
+            protocol: protocol.to_string(),
+            host: host.to_string(),
+            port,
+        }
     }
 }
 
 impl ToString for EhClientProxy {
     /// 将 EhClientProxy 转换为 URL 字符串
     fn to_string(&self) -> String {
-        format!("{}://{}:{}", self.0, self.1, self.2)
+        format!("{}://{}:{}", self.protocol, self.host, self.port)
     }
 }
