02b07a4261f1a2bf37fe385fd9274b5657fd0500
pboymt <10893582+pboymt@users.noreply.github.com>
2024-02-15 16:42:45 +0800

    feat: 添加客户端结构体实例化设置与请求方法

--- src/client/client.rs
+++ src/client/client.rs
@@ -1,34 +1,67 @@
-use reqwest::{Client, Proxy, Url};
+use reqwest::{cookie::Jar, redirect, Client, Proxy, Url};
 use serde::de::DeserializeOwned;
 
-#[derive(Debug, Clone)]
-pub struct EhClientProxy(String, String, i32);
+use crate::dto::{enums::Offset, keyword::Keyword, site::Site};
+use crate::url::search::SearchBuilder;
 
-impl EhClientProxy {
-    pub fn new(schema: &str, host: &str, port: i32) -> Self {
-        EhClientProxy(schema.to_string(), host.to_string(), port)
-    }
-}
+use super::config::EhClientConfig;
 
-#[derive(Debug, Clone)]
+#[derive(Clone)]
 pub struct EhClient {
+    site: Site,
     client: Client,
 }
 
 impl EhClient {
-    pub fn new(proxy: Option<EhClientProxy>) -> Self {
-        let mut builder = Client::builder();
-        if let Some(proxy) = proxy.clone() {
-            let proxy = Proxy::all(format!("{}://{}:{}", proxy.0, proxy.1, proxy.2));
+    pub fn new(config: EhClientConfig) -> Self {
+        let mut builder = Client::builder()
+            .redirect(redirect::Policy::limited(20))
+            .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36 Edg/121.0.0.0");
+        if let Some(proxy) = config.proxy.clone() {
+            let proxy = Proxy::all(proxy.to_string());
             if let Ok(proxy) = proxy {
                 builder = builder.proxy(proxy);
             }
         }
+        if let Some(auth) = config.auth.clone() {
+            let jar = Jar::default();
+            let auth_vec = auth.to_vec();
+            for (key, value) in auth_vec {
+                jar.add_cookie_str(&format!("{}={}", key, value), &config.site.into());
+            }
+            println!("Cookie: {:?}", jar);
+            builder = builder.cookie_store(true).cookie_provider(jar.into());
+        }
+
         let client = builder.build();
+
         match client {
-            Ok(client) => EhClient { client },
+            Ok(client) => EhClient {
+                client,
+                site: config.site,
+            },
             Err(err) => panic!("Error: {}", err),
         }
+    }
+
+    /// 不包含高级选项的搜索
+    pub async fn search(
+        &self,
+        keywords: Vec<Keyword>,
+        offset: Option<Offset>,
+    ) -> Result<String, String> {
+        let mut builder = SearchBuilder::new(self.site).add_keywords(keywords);
+        if let Some(offset) = offset {
+            builder = builder.offset(offset);
+        }
+        let url = builder.build()?;
+        let text = match self.get_html(url).await {
+            Ok(text) => text,
+            Err(err) => {
+                return Err(format!("Error: {}", err));
+            }
+        };
+        Ok(text)
     }
 
     pub async fn get_html(&self, url: Url) -> Result<String, String> {
@@ -71,24 +104,36 @@ impl EhClient {
 
 #[cfg(test)]
 mod tests {
-    use tokio::{fs::File, io::AsyncWriteExt};
+    use crate::{
+        client::{config::EhClientConfig, proxy::EhClientProxy},
+        dto::{keyword::Keyword, site::Site},
+    };
 
-    use super::{EhClient, EhClientProxy};
+    use super::EhClient;
+    use tokio::{fs::File, io::AsyncWriteExt};
 
     #[tokio::test]
     async fn test_eh_client() {
         let proxy = EhClientProxy::new("http", "127.0.0.1", 7890);
-        let client = EhClient::new(Some(proxy));
-        let res = client.client.get("https://e-hentai.org").send().await;
+        let config = EhClientConfig {
+            site: Site::Eh,
+            proxy: Some(proxy),
+            auth: None,
+        };
+        let client = EhClient::new(config);
+        let res = client
+            .search(
+                vec![
+                    Keyword::Artist("simon".into()),
+                    Keyword::Language("chinese".into()),
+                ],
+                None,
+            )
+            .await;
         match res {
-            Ok(res) => {
-                assert_eq!(res.status(), 200);
-                let text = res.text().await;
-                let text = match text {
-                    Ok(text) => text,
-                    Err(err) => panic!("Error: {}", err),
-                };
-                let file = File::create("samples/index.html").await;
+            Ok(text) => {
+                println!("HTML: {}", text);
+                let file = File::create("samples/search.html").await;
                 let mut file = match file {
                     Ok(file) => file,
                     Err(err) => panic!("Error: {}", err),
--- src/client/config.rs
+++ src/client/config.rs
@@ -1,9 +1,11 @@
-use crate::url::enums::Site;
+use serde::{Deserialize, Serialize};
+
+use crate::dto::site::Site;
 
 use super::{auth::EhClientAuth, proxy::EhClientProxy};
 
 /// E-Hentai/ExHentai 客户端配置
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct EhClientConfig {
     /// 站点类型
     pub site: Site,
@@ -21,5 +23,65 @@ impl Default for EhClientConfig {
             proxy: None,
             auth: None,
         }
+    }
+}
+
+mod tests {
+
+    #[test]
+    fn config_to_json() {
+        use crate::client::{config::EhClientConfig, proxy::EhClientProxy};
+        let mut config = EhClientConfig::default();
+        let proxy = EhClientProxy::new("http", "127.0.0.1", 7890);
+        config.proxy = Some(proxy);
+        let result = serde_json::to_string(&config);
+        assert!(result.is_ok());
+        println!("{}", result.unwrap());
+    }
+
+    #[test]
+    fn config_to_yaml() {
+        use crate::client::{auth::EhClientAuth, config::EhClientConfig, proxy::EhClientProxy};
+        let mut config = EhClientConfig::default();
+        let proxy = EhClientProxy::new("http", "127.0.0.1", 7890);
+        config.proxy = Some(proxy);
+        let auth = EhClientAuth::new("", "", Some(""));
+        config.auth = Some(auth);
+        let result = serde_yaml::to_string(&config);
+        assert!(result.is_ok());
+        println!("{}", result.unwrap());
+    }
+
+    #[test]
+    fn config_from_json() {
+        use crate::client::config::EhClientConfig;
+        let config = r#"
+{
+    "site": "ex",
+    "proxy": {
+        "protocol": "http",
+        "host": "127.0.0.1",
+        "port": 7890
+    }
+}
+"#;
+        let result = serde_json::from_str::<EhClientConfig>(config);
+        assert!(result.is_ok());
+        println!("{:?}", result.unwrap());
+    }
+
+    #[test]
+    fn config_from_yaml() {
+        use crate::client::config::EhClientConfig;
+        let config = r#"
+site: ex
+proxy:
+  protocol: http
+  host: 127.0.0.1
+  port: 7890
+"#;
+        let result = serde_yaml::from_str::<EhClientConfig>(config);
+        assert!(result.is_ok());
+        println!("{:?}", result.unwrap());
     }
 }
